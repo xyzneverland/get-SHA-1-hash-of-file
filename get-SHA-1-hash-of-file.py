ee1793e6acb3832b50bd9b89f4d07379a7b0d639
xyzneverland <120294211+xyzneverland@users.noreply.github.com>
2023-03-22 19:19:08 +0100

    Read the file in binary mode instead of encoding.
    The changes I made aims to read the file as a binary file instead of using a specific encoding, which should lead to more consistent hash results that match other hashing tools.
    Modify the read_file() function to open and read the file in binary mode ("rb") instead of using an encoding.
    Remove the encoding parameter from the read_file() function, as it's no longer needed.
    Read the file contents as binary data.
    Update the hash object with the binary data directly, without encoding it.
    By reading the file as binary data, the need to specify an encoding is bypassed, and the script should provide more consistent results that align with other hashing tools(tested).

--- get-SHA-1-hash-of-file.py
+++ get-SHA-1-hash-of-file.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-#SHA-1 hash of the file v1.1
-#by xyzneverland
+#SHA-1 hash of the file v1.2 (https://github.com/xyzneverland/get-SHA-1-hash-of-file/)
+#by xyzneverland (https://github.com/xyzneverland/)
 import hashlib
 import os
 import time
 
-def read_file(file_name, encoding="utf-8"):
-  with open(file_name, "r", encoding=encoding) as f:
+def read_file(file_name):
+  with open(file_name, "rb") as f:
     return f.read()
 
 file_name = input("Please enter the file name: ")
@@ -14,11 +14,9 @@ file_name = input("Please enter the file name: ")
 while not os.path.exists(file_name):
   file_name = input("File not found. Please enter the file name again: ")
 
-file_contents = read_file(file_name, encoding="latin1")
-
+file_contents = read_file(file_name)
 sha1 = hashlib.sha1()
-file_contents_encoded = file_contents.encode(encoding="latin1")
-sha1.update(file_contents_encoded)
+sha1.update(file_contents)
 file_hash = sha1.hexdigest()
 
 print(f"The SHA-1 hash of the file '{file_name}' is: {file_hash}")
